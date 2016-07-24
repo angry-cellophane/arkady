5f86cd72b8cd6a8265ecd7075566b1aa4189308f
Alexander Kazakov <kazakov.a.s.90@gmail.com>
2016-07-25 00:51:54 +0300

    Tricky hack ot avoid class cast exception. Compile static + hacking closure delegates is not a good idea since groovy tries to calculate exact type at the compile time, but we're hacking the types.

--- src/main/groovy/org/ka/arkady/spec/MatchBodySpec.groovy
+++ src/main/groovy/org/ka/arkady/spec/MatchBodySpec.groovy
@@ -2,7 +2,10 @@ package org.ka.arkady.spec
 
 import groovy.transform.stc.ClosureParams
 import groovy.transform.stc.SimpleType
+import org.ka.arkady.aggregator.Aggregator
 
 interface MatchBodySpec {
     AfterWhenSpec when(Object assertion)
+    Aggregator newAggregator(Map<String, Object> params)
+    Aggregator findByName(String aggregatorName)
 }
